02b392a3f3a8874cdb650a268da39a2cb165afa7
=greengerong <=grzx2210@163.com>
2013-01-12 11:42:10 +0800

    create project

---
+++ src/main/java/com/thoughtworks/TWCITask/App.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.TWCITask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/thoughtworks/TWCITask/AppTest.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.TWCITask;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+
+public class AppTest
+
+{
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+}
