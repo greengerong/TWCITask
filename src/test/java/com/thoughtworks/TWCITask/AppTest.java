07b2c3ca1595b8cb8fbcf832ffdad63b3afe2ba7
=greengerong <=grzx2210@163.com>
2013-01-12 13:21:02 +0800

    fail

--- src/test/java/com/thoughtworks/TWCITask/AppTest.java
+++ src/test/java/com/thoughtworks/TWCITask/AppTest.java
@@ -3,20 +3,21 @@ package com.thoughtworks.TWCITask;
 import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
 
 
 public class AppTest
 
 {
+    private  App app;
     @Before
     public void setUp() {
-
+        app = new App();
     }
 
     @Test
     public void testApp() {
-        assertTrue(false);
+        assertEquals(app.getBool(), true);
     }
 
 }
